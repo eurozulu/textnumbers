9a1d1c2b2c3a792936404ec1aa127de67493e1e0
Rob Gilham <rob.gilham@loomispay.com>
2023-10-16 14:17:50 +0200

    fixed main Added french

---
+++ README.md
@@ -0,0 +1,12 @@
+# Text Numbers
+## Convert numbers into written form  
+#### Overview
+A simple application which takes a single decimal number and converts it into its writenform.  
+The application is not language specific, and aims to be able to support a wide variety of languages.
+  
+#### Usage
+`tn 1234`  
+Outputs: `one thousand two hundred and thirty four`  
+
+`tn 1234 as french`  
+Outputs: `one thousand two hundred and thirty four`  
--- main/tn/main.go
+++ main/tn/main.go
@@ -4,31 +4,54 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 	"textnumbers"
 )
 
+const defaultLanguage = "english"
+
 func main() {
-	l, err := textnumbers.OpenLanguage("english")
+	n, lang, err := readArgs(os.Args[1:])
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	l, err := textnumbers.OpenLanguage(lang)
 	if err != nil {
 		log.Println(err)
 		return
 	}
+	fmt.Println(l.Format(n))
+}
 
-	fmt.Println(l.Title())
-
-	if true {
-		return
+func readArgs(args []string) (int64, string, error) {
+	if len(args) < 1 {
+		return 0, "", fmt.Errorf("provide a number to convert")
 	}
-	args := asArgs(os.Args[1:])
-	p := args.Parameters()
-	if len(p) != 1 {
-		fmt.Println("Give a number as a parameter")
-		return
+	i, err := strconv.ParseInt(args[0], 10, 64)
+	if err != nil {
+		return 0, "", err
 	}
-	//i, err := strconv.Atoi(p[0])
-	//if err != nil {
-	//	fmt.Printf("failed to read parameter as a  number  %v", err)
-	//	return
-	//}
+	var lang string
+	ix := findIndex("as", args)
+	if ix > 0 {
+		if ix+1 >= len(args) {
+			return 0, "", fmt.Errorf(("must provide a language name."))
+		}
+		lang = args[ix+1]
+	} else {
+		lang = defaultLanguage
+	}
+	return i, lang, nil
+}
 
+func findIndex(s string, ss []string) int {
+	for i, sz := range ss {
+		if strings.EqualFold(sz, s) {
+			return i
+		}
+	}
+	return -1
 }
